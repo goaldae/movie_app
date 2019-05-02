b37995e6fcefbe8357c43ebba4909d5b0db67ff7
goaldae <goaldae@naver.com>
2019-05-02 14:45:45 +0900

    Validating props with prop types

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Movie extends Component{
+    static propTypes = {
+        title : PropTypes.string.isRequired,
+        poster : PropTypes.string.isRequired,
+    }
+
     render() {
         return(
             <div className="Movie">
@@ -12,6 +18,10 @@ class Movie extends Component{
 }
 
 class MoviePoster extends Component{
+    static propTypes = {
+        poster : PropTypes.string.isRequired,
+    }
+
     render() {
         return(
             <div className="MoviePoster">
