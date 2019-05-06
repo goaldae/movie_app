23cba76f20f364d276fc7fb3e156fe15596ea04f
goaldae <goaldae@naver.com>
2019-05-06 13:22:46 +0900

    Make Loading state

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from "./Movie";
 
+
+
 class App extends Component{
     state = {
-        movies : [
-            {title : "Avengers", poster : "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg"},
-            {title : "Matrix", poster : "https://images-na.ssl-images-amazon.com/images/I/91wkDmLK3UL._RI_.jpg"},
-            {title : "Iron Man3", poster : "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/55/Iron_Man_3_IMAX_poster.jpg/revision/latest?cb=20130319165854"},
-        ]
+    }
+
+    _roadMovies = () => {
+        const movies = this.state.movies.map((movie, index) => {
+            return <Movie title = {movie.title} poster = {movie.poster} key = {index} />
+        })
+        return movies;
     }
 
   render() {
     return (
         <div className="App">
-            {this.state.hello}
-            {this.state.movies.map((movie, index) => {
-                return <Movie title = {movie.title} poster = {movie.poster} key = {index} />
-            })}
-
+            {this.state.movies ? this._roadMovies() : "roading"}
         </div>
 
     );
@@ -27,12 +27,11 @@ class App extends Component{
   componentDidMount() {
         setTimeout(()=>{
             this.setState({
-                hello :'hello',
                 movies : [
-                    ...this.state.movies,
-                    {
-                        title : "loveholic", poster : "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg"
-                    }
+                    {title : "Avengers", poster : "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg"},
+                    {title : "Matrix", poster : "https://images-na.ssl-images-amazon.com/images/I/91wkDmLK3UL._RI_.jpg"},
+                    {title : "Iron Man3", poster : "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/55/Iron_Man_3_IMAX_poster.jpg/revision/latest?cb=20130319165854"},
+                    {title : "loveholic", poster : "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Loveholic%2C_2005_TV_series.jpg/250px-Loveholic%2C_2005_TV_series.jpg"}
                 ]
             })
         }, 2000)
