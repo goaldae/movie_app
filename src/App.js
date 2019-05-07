e7de2bf22984a604fdb074fca995d4e1bd3aa348
goaldae <goaldae@naver.com>
2019-05-07 13:38:35 +0900

    AJAX on React

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ class App extends Component{
     state = {
     }
 
+    componentDidMount(){
+        fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+            .then(response => response.json())
+            .then(json => { console.log(json.data.movies);return this.setState({movies : json.data.movies})})
+            .catch(error => console.log(error))
+    }
+
     _roadMovies = () => {
         const movies = this.state.movies.map((movie, index) => {
-            return <Movie title = {movie.title} poster = {movie.poster} key = {index} />
+            return <Movie title = {movie.title} poster = {movie.medium_cover_image} key = {index} />
+
         })
         return movies;
     }
@@ -24,18 +32,6 @@ class App extends Component{
     );
   }
 
-  componentDidMount() {
-        setTimeout(()=>{
-            this.setState({
-                movies : [
-                    {title : "Avengers", poster : "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg"},
-                    {title : "Matrix", poster : "https://images-na.ssl-images-amazon.com/images/I/91wkDmLK3UL._RI_.jpg"},
-                    {title : "Iron Man3", poster : "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/55/Iron_Man_3_IMAX_poster.jpg/revision/latest?cb=20130319165854"},
-                    {title : "loveholic", poster : "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Loveholic%2C_2005_TV_series.jpg/250px-Loveholic%2C_2005_TV_series.jpg"}
-                ]
-            })
-        }, 2000)
-  }
 
 }
 
